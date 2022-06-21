2877f8553c625252a006563725060456e96f53c0
Fabio dos santos <fah_ds@live.com>
2022-06-21 15:22:01 -0300

    feat: add connection with graphcms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from 'react';
+import react, { useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+      teacher {
+        id
+        name
+      }
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+  teacher: {
+    id: string;
+    name: string;
+  };
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
 
   return (
-      <h1 className="text-2xl font-bold text-violet-500">Hello </h1>
+    <ul>
+      {data?.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
   );
 }
 
